e2328da7b29cbe772aea1e64e5dcd010230e43dd
Yasin Khoshbakht <yasinkhoshbakht1400@gmail.com>
2026-07-02 14:42:32 +0330

    Fix empty singlePage

--- src/Pages/SingleRecepe.jsx
+++ src/Pages/SingleRecepe.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SingleRecepe() {
-  return <div>SingleRecepe</div>;
+  return <div className="w-full h-[400px] text-primaryColor font-serif font-bold text-8xl flex items-center justify-center">Coming Soon.....</div>;
 }
 
 export default SingleRecepe;
